e74ad40bba72eb2ef9c65023dcc65c23f8a7d93f
techpixel <68567672+techpixel@users.noreply.github.com>
2025-02-03 10:25:31 -0600

    add inital flow logic

--- src/events/userJoinedHuddle.ts
+++ src/events/userJoinedHuddle.ts
@@ -1,23 +1,28 @@
 import { prisma } from "../util/prisma";
 import { app } from "../util/bolt";
 import type { Huddle } from "../util/bolt";
-import { config } from "../util/transcript";
+import { config, t } from "../util/transcript";
 
 export default async (args: {
     slackId: string,
     huddle: Huddle
 }) => {
-    // await prisma.call.create({
-    //     data: {
-    //         user: { connect: { slackId: args.slackId } },
-    //         inCall: true,
-    //         joinedAt: new Date(),
-    //         callId: args.huddle.call_id
-    //     }
-    // });
-    await app.client.chat.postMessage({
-        token: process.env.SLACK_BOT_TOKEN,
-        channel: config.CAFE_CHANNEL,
-        text: `<@${args.slackId}> joined the huddle! There are ${args.huddle.active_members.length} other people.`
+    /*
+
+    Preemptively create a session for the user joining the huddle. This is how we will track the user as they progress through the flow
+
+    */
+    await prisma.session.create({
+        data: {
+            user: { connect: { slackId: args.slackId } },
+            callId: args.huddle.call_id,
+            joinedAt: new Date(),
+        }
+    });
+
+    await app.client.chat.postEphemeral({
+        channel: args.huddle.channel_id,
+        user: args.slackId,
+        text: t('huddle_join')
     });
 }

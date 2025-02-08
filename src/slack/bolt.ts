847d4fc1fbe30f591c3d7098ea3af587b907f1ae
techpixel <68567672+techpixel@users.noreply.github.com>
2025-02-07 23:53:01 -0600

    add health check

--- src/slack/bolt.ts
+++ src/slack/bolt.ts
@@ -1,8 +1,25 @@
-import { App } from '@slack/bolt';
+import { App, ExpressReceiver } from '@slack/bolt';
+import { Config } from '../config';
+
+const expressReciever = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET!,
+  endpoints: '/slack/events',
+})
+
+expressReciever.app.get('/health', async (req, res) => {
+  await app.client.chat.postMessage({
+    channel: Config.LOGS_CHANNEL,
+    text: 'recieved health check!'
+  });
+
+  res.sendStatus(200);
+});
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
+
+
 
 export { app };

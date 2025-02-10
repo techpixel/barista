f33b73d555ebfccc215d8eb5604fa9ce03759980
techpixel <68567672+techpixel@users.noreply.github.com>
2025-02-09 23:04:26 -0600

    fix command

--- src/commands/myCups.ts
+++ src/commands/myCups.ts
@@ -2,7 +2,7 @@ import { app } from "../slack/bolt";
 import huddleKick from "../slack/huddleKick";
 import { prisma } from "../util/prisma";
 
-app.command('/kick-from-call', async ({ ack, payload }) => {
+app.command('/my-cups', async ({ ack, payload }) => {
     await ack();
 
     try {

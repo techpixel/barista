1412e30d104cec0a6df93394223ecdae7e2acce9
techpixel <68567672+techpixel@users.noreply.github.com>
2025-02-23 16:21:59 -0600

    remove old files

--- old/oldsessions/isPaused.ts
+++
@@ -1,8 +0,0 @@
-// Checks if a session is paused
-
-import type { Session } from "@prisma/client";
-// import { prisma } from "../util/prisma";
-
-export default (session: Session) => {
-    return session.paused;
-}
